3105f82ca8a1d51597a290f7777a03449cb3280f
twell1337 <vtselishchevo@gmail.com>
2024-07-01 19:38:18 +0700

    Started

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Разрешить доступ к статическим файлам из папки dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 // Отправка главной HTML страницы
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+  res.sendFile(path.join(__dirname, '/src/index.html'));
+});
+
+// Webpack в режиме наблюдения за изменениями main.js
+const webpackProcess = spawn('npm', ['run', 'watch']);
+webpackProcess.stdout.on('data', (data) => {
+  console.log(`Webpack stdout: ${data}`);
+});
+webpackProcess.stderr.on('data', (data) => {
+  console.error(`Webpack stderr: ${data}`);
 });
 
 // Запуск сервера
